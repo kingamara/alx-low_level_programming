1250e3ba449ced9cfd60fa74b90fb4dd948cb1a3
kingamara <adimeme1@yahoo.com>
2023-02-21 13:01:53 -0800

    a function that prints 10 times the alphabet, in lowercase, followed by a new line.

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,7 +5,7 @@
  * Return: 0 (success)
  */
 
-void print_alphabet_x10(void) /* this code prints alphabet in roles of 10 */
+void print_alphabet_x10(void) /* this code prints alphabet in 10 roles. */
 {
 
 char c;

503ee27e75fc620c06ac1c512227c0ef62f682f4
Amara <67520641+kingamara@users.noreply.github.com>
2023-02-21 14:02:40 -0600

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,12 @@
  *
  * Return: Always 0.
  */
-
 void print_alphabet(void)
 {
-char c;
-for (c = 'a' ; c<= 'z'; c++)
+  char c;
+  for (c = 'a' ; c<= 'z'; c++)
 {
-_putchar(c);
+  _putchar(c);
 }
-_putchar('\n');
+  _putchar('\n');
 }

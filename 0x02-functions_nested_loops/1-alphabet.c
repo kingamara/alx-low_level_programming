f14112948a6a613536876c5a1caca3edcabd2d64
Amara <67520641+kingamara@users.noreply.github.com>
2023-02-22 20:55:53 -0600

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,16 +6,6 @@
  * Return: 0 (success)
  */
 void print_alphabet(void) /* print_alphabet prints the alphabet in lowercase */
-<<<<<<< HEAD
-{char alpha;
-
-  alpha = 'a';
-  while (alpha <= 'z')
-    {_putchar(alpha);
-      alpha++;
-    }
-  _putchar('\n');
-=======
 {	char alpha;
 
 	alpha = 'a';
@@ -24,5 +14,4 @@ void print_alphabet(void) /* print_alphabet prints the alphabet in lowercase */
 		alpha++;
 	}
 	_putchar('\n');
->>>>>>> 26c7f28ada77b38a3c6b56c4d7e0bdb4e0ed4d9f
 }

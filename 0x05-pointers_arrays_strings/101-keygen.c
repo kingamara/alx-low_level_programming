6d4e9fa9d91a46f1f5a57c388778c03c8d000429
Amara <67520641+kingamara@users.noreply.github.com>
2023-02-27 20:10:32 -0600

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -5,13 +5,13 @@
 #define PASSWORD_LENGTH 8
 
 int main() {
-char password[PASSWORD_LENGTH + 1]; // +1 for null terminator
+char password[PASSWORD_LENGTH + 1]; /* +1 for null terminator*/
 const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-srand(time(NULL)); // Seed random number generator with current time
+srand(time(NULL)); /*Seed random number generator with current time*/
 for (int i = 0; i < PASSWORD_LENGTH; i++) {
 password[i] = charset[rand() % sizeof(charset)];
 }
-password[PASSWORD_LENGTH] = '\0'; // Add null terminator to end of password string
+password[PASSWORD_LENGTH] = '\0'; /* Add null terminator to end of password string*/
 printf("Generated password: %s\n", password);
 return 0;
 }
